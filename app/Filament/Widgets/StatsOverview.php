2c667e3ece8a1ab1e627df49d1ec2f13c70ae7ef
Mardin Nasouti <info@mardin.cc>
2025-10-21 12:09:01 +0300

    feat(dashboard): total sell and orders count stats

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends StatsOverviewWidget
+{
+    protected static ?int $sort = 1;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Sell', '192 TRY'),
+            Stat::make('Orders count', '12'),
+        ];
+    }
+}
